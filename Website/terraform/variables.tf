c974f5886abeddb2ba43d8f4cb69c6baf61cd86c
Mate Kovacs <mateoliverkovacs@gmail.com>
2023-12-30 10:04:34 +0100

    twst

--- Website/terraform/variables.tf
+++ Website/terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "access_key" {
+variable "AWS_ACCESS_KEY_ID" {
   description = "Access_key"
 }
 
-variable "secret_access_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
   description = "secret_access_key"
 }
